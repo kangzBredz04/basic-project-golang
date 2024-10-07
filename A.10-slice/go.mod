ad80582ca1cc5ca23e349d27385713dc237f5f96
kangzBredz04 <wabredz1234@gmail.com>
2024-10-07 11:04:28 +0700

    feat: add folder and files for learning maps in Golang

---
+++ A.10-slice/go.mod
@@ -0,0 +1,3 @@
+module A.10-slice
+
+go 1.23.2
---
+++ A.10-slice/slice.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := make([]int, 0)   // Slice kosong
+	slice = append(slice, 10) // Menambahkan elemen
+	slice = append(slice, 20)
+	slice = append(slice, 30)
+
+	fmt.Println("Slice:", slice) // Output: [10 20 30]
+
+	var fruits = []string{"apple", "grape", "banana", "melon"}
+	var aFruits = fruits[0:3]
+	var bFruits = fruits[1:4]
+	var aaFruits = aFruits[1:2]
+	var baFruits = bFruits[0:1]
+
+	fmt.Println(fruits)   // [apple grape banana melon]
+	fmt.Println(aFruits)  // [apple grape banana]
+	fmt.Println(bFruits)  // [grape banana melon]
+	fmt.Println(aaFruits) // [grape]
+	fmt.Println(baFruits) // [grape]
+	// Buah "grape" diubah menjadi "pinnaple"
+	baFruits[0] = "pinnaple"
+
+	fmt.Println(fruits)   // [apple pinnaple banana melon]
+	fmt.Println(aFruits)  // [apple pinnaple banana]
+	fmt.Println(bFruits)  // [pinnaple banana melon]
+	fmt.Println(aaFruits) // [pinnaple]
+	fmt.Println(baFruits) // [pinnaple]
+}
